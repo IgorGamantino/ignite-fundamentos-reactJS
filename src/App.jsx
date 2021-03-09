77060f95ccacb3c261e0472b5aa7192b6f4f7a79
Igor Amantino <justin.amantino@gmail.com>
2021-03-09 10:58:38 -0300

    conceitos de estado do componente

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
+import Counter from './components/counter';
 import  {RepositoryList}  from './components/RepositoryList';
 import global from './styles/global.scss'
 // import { Container } from './styles';
 
 export function App(){
-  return <RepositoryList />
+  return(
+    <>
+     <RepositoryList />
+     <Counter />
+    </>
+    )
 }
---
+++ src/components/counter.jsx
@@ -0,0 +1,16 @@
+import { useState } from "react"
+
+export default function Counter(){
+ const [counter,setCounter] = useState(0)
+
+ function icrementCounter() {
+   setCounter(counter + 2)
+ }
+
+  return (
+     <div>
+       <h2>{counter}</h2>
+       <button type='button' onClick={icrementCounter}>Icrement {counter}</button>
+     </div>
+  )
+}
